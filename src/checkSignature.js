59f2c0edb1b569c2ade6fe3862b30ca3ed77c804
Dr4goniez <dragoniez.jawp@gmail.com>
2023-12-20 07:10:29 +0900

    +checkSignature.js

---
+++ src/checkSignature.js
@@ -0,0 +1,122 @@
+/*!*
+ * 署名忘れ防止スクリプト [[利用者:Cpro|cpro]] 2012年12月6日 (木) 07:39 (UTC)
+ *
+ * 以下のスクリプトはパブリックドメインとします。
+ * 改変・再配布を含め自由にお使いいただけますが、自己責任でお願いします。
+ * This script is under the public domain.
+ * You can freely use, modify, or redistribute it at your own risk.
+ *
+ * Modified in December 2023 by [[User:Dragoniez]]
+ */
+/* global mw, OO */
+//<nowiki>
+
+$.when(mw.loader.using(['oojs-ui-core', 'oojs-ui-windows']), $.ready).then(function() {
+
+	// 編集またはプレビュー時にスクリプトを実行
+	if (['edit', 'submit'].indexOf(mw.config.get('wgAction')) === -1) return;
+
+	// すべてのノート名前空間と、Wikipedia名前空間、プロジェクト名前空間の一部を対象とする
+	var ns = mw.config.get('wgNamespaceNumber');
+	if (ns < 0 || ns % 2 === 0 && [4, 102].indexOf(ns) === -1) return;
+
+	// 条件付き名前空間の場合スクリプトを走らせるページ名を取得
+	var rTitles;
+	switch (ns) {
+		case 4:
+			rTitles = [
+				'^井戸端($|/subj/)',
+				'^削除依頼/(?!ログ/)',
+				'^リダイレクトの削除依頼/受付$',
+				'^(削除の復帰|投稿ブロック|チェックユーザー)依頼/',
+				'^(保護(解除)?|移動|利用者ページの削除|著作権問題調査)依頼$',
+				'^(改名|統合|分割)提案$',
+				'^(ガジェット|編集フィルター)/提案$',
+				'^管理者伝言板/(投稿ブロック|保護ページ編集|その他の伝言)($|/)'
+			];
+			break;
+		case 102:
+			rTitles = [
+				'^カテゴリ関連/議論/'
+			];
+			break;
+		default:
+	}
+
+	// 取得したページ名があり、そのどれにもマッチしない場合終了
+	if (rTitles && !new RegExp(rTitles.join('|')).test(mw.config.get('wgTitle'))) return;
+
+	// 「変更を公開」が押された時
+	$('#wpSave').off('click').on('click', function(e) {
+
+		// 細部の編集のチェック状態を取得
+		var isMinorEdit = $('#wpMinoredit').prop('checked');
+
+		// 「細部の編集にチェックを入れたときは署名がなくてもポップアップを表示しない」ガジェットが有効か
+		var suppressWhenMinor = mw.loader.getState('ext.gadget.checkSignature-suppressWhenMinor') === 'ready';
+
+		// 細部の編集がチェックされ、かつ確認抑制ガジェットが有効であれば終了
+		if (isMinorEdit && suppressWhenMinor) return;
+
+		// 署名の有無をチェック
+		var text = $('#wpTextbox1').val();
+		if (typeof text !== 'string') return;
+
+		// 署名がある場合
+		var rSig = /[^~]~~~~(?!~)/; // チルダ4つ（それ以外の個数はNG）
+		if (/^\s*~~~~(?!~)/.test(text)) { // 本文先頭に署名がある場合は上の正規表現がカバーできないので念のため
+			return;
+		} else if (rSig.test(text)) {
+
+			// 署名がコメントまたはnowiki内にないことを保障
+			var rTag = {
+				comment: { // 以下、C
+					start: /^<!--/,
+					end: /^-->/
+				},
+				nowiki: { // 以下、N
+					start: /^<nowiki[^>\n]*>/,
+					end: /^<\/nowiki[^>\n]*>/
+				}
+			};
+			var rClose, m;
+			for (var i = 0; i < text.length; i++) { // 本文の1文字目から順番にチェック
+
+				// i文字目から最後までのウィキテキスト
+				var substr = text.slice(i);
+
+				// C内でもN内でもない時に署名を見つけたら終了
+				if (!rClose && substr.search(rSig) === 0) {
+					return;
+
+				// C内でもN内でもない時にCかNの開始タグを見つけたら、探す終了タグの正規表現を登録
+				} else if (!rClose) {
+					if ((m = rTag.comment.start.exec(substr))) {
+						rClose = rTag.comment.end;
+						i += m[0].length - 1;
+					} else if ((m = rTag.nowiki.start.exec(substr))) {
+						rClose = rTag.nowiki.end;
+						i += m[0].length - 1;
+					}
+
+				// C内かN内で対応する閉じタグをを見つけたら、探す終了タグの正規表現をリセット
+				} else if (rClose && (m = rClose.exec(substr))) {
+					rClose = void 0;
+					i += m[0].length - 1;
+				}
+
+			}
+
+		}
+
+		// コードがここまでたどり着いた場合署名がないため警告し、OKが押されなかったら保存処理をキャンセル
+		// (OO.ui.confirmが非同期処理のため先に保存処理をキャンセルし、OKが押されたらフォームをsubmit)
+		e.preventDefault();
+		OO.ui.confirm('署名が入力されていません。このまま投稿しますか？').then(function(confirmed) {
+			if (confirmed) $('#editform').trigger('submit');
+		});
+
+	});
+
+});
+//</nowiki>
