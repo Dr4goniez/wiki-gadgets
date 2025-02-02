484d1b22489186b94d3ed5a1c0d7c9ee67a24ecb
Dr4goniez <dragoniez.jawp@gmail.com>
2025-02-02 18:00:12 +0900

    MRD v3: Add Revision class, constructor almost completed

--- src/MassRevisionDelete3.js
+++ src/MassRevisionDelete3.js
@@ -43,18 +43,13 @@ if (!rights.revdel) {
 }
 const apilimit = rights.AHL ? 500 : 50;
 
-const cls = {
-	deleted: 'history-deleted',
-	suppressed: 'mw-history-suppressed'
-};
-
 //*********************************************************************************************
 
 /** @type {mw.Api} */
 let api;
 function init() {
 	$.when(
-		mw.loader.using(['mediawiki.api', 'oojs-ui', 'oojs-ui.styles.icons-movement']),
+		mw.loader.using(['mediawiki.api', 'jquery.makeCollapsible', 'oojs-ui', 'oojs-ui.styles.icons-movement']),
 		$.ready
 	).then(() => {
 
@@ -83,8 +78,8 @@ function init() {
 		createStyleTag();
 		const fieldset = createFieldset($contribsList);
 
-		const form = new RevdelForm(fieldset);
-		form.initializeReasonDropdowns();
+		const mrd = new MassRevisionDelete(fieldset, $contribsList);
+		mrd.initializeReasonDropdowns();
 
 	});
 }
@@ -95,10 +90,21 @@ function createStyleTag() {
 		'.mrd-wrapper .oo-ui-fieldLayout-header {' +
 			'font-weight: bold;' +
 		'}' +
-		// Class to align block-level widgets horizontally
-		'.mrd-horizontal > * {' +
+		'.mrd-horizontal > * {' + // Class to align block-level widgets horizontally
 			'display: inline-block;' +
 			'margin-right: 1em;' +
+		'}' +
+		'.mrd-progress:not(:empty) {' +
+			'margin-right: 0.5em;' +
+		'}' +
+		'.mrd-checkbox {' +
+			'margin-right: 0.5em;' +
+		'}' +
+		'.mrd-green {' +
+			'color: mediumseagreen;' +
+		'}' +
+		'.mrd-red {' +
+			'color: mediumvioletred;' +
 		'}';
 	document.head.appendChild(style);
 }
@@ -112,7 +118,7 @@ function createFieldset($contribsList) {
 
 	// Create a collapsible fieldset layout
 	/**
-	 * Adapted from:
+	 * See also:
 	 * @link https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/core/+/refs/heads/master/includes/htmlform/CollapsibleFieldsetLayout.php
 	 */
 	const wrapper = new OO.ui.PanelLayout({
@@ -146,7 +152,7 @@ function createFieldset($contribsList) {
 			.addClass('mw-collapsible-content');
 
 	$contribsList.eq(0).before(wrapper.$element);
-	mw.hook('wikipage.content').fire(wrapper.$element); // Make the fieldset collapsible
+	fieldset.$element.makeCollapsible();
 
 	return fieldset;
 
@@ -206,12 +212,19 @@ class VisibilityLevel {
 
 }
 
-class RevdelForm {
+class MassRevisionDelete {
 
 	/**
 	 * @param {OO.ui.FieldsetLayout} fieldset
+	 * @param {JQuery<HTMLUListElement>} $contribsList
 	 */
-	constructor(fieldset) {
+	constructor(fieldset, $contribsList) {
+
+		/**
+		 * @type {Revision[]}
+		 */
+		this.list = Array.from($contribsList.children('li')).map((li) => new Revision(li));
+		console.log(this.list);
 
 		/**
 		 * @type {VisibilityLevel}
@@ -239,14 +252,26 @@ class RevdelForm {
 		this.reasonC = new OO.ui.TextInputWidget({
 			placeholder: '他の、または追加の理由'
 		});
-		this.btnSelectAll = new OO.ui.ButtonWidget({
+		const btnSelectAll = new OO.ui.ButtonWidget({
 			label: '全選択'
+		}).off('click').on('click', () => {
+			this.list.forEach((Rev) => {
+				Rev.toggleSelection(true);
+			});
 		});
-		this.btnUnelectAll = new OO.ui.ButtonWidget({
+		const btnUnselectAll = new OO.ui.ButtonWidget({
 			label: '全選択解除'
+		}).off('click').on('click', () => {
+			this.list.forEach((Rev) => {
+				Rev.toggleSelection(false);
+			});
 		});
-		this.btnInvertSelection = new OO.ui.ButtonWidget({
+		const btnInvertSelection = new OO.ui.ButtonWidget({
 			label: '選択反転'
+		}).off('click').on('click', () => {
+			this.list.forEach((rev) => {
+				rev.toggleSelection(!rev.isSelected());
+			});
 		});
 		this.btnExecute = new OO.ui.ButtonWidget({
 			label: '選択された版に適用',
@@ -261,7 +286,7 @@ class RevdelForm {
 			new OO.ui.FieldLayout(this.reason2),
 			new OO.ui.FieldLayout(this.reasonC),
 			new OO.ui.FieldLayout(new OO.ui.ButtonGroupWidget({
-				items: [this.btnSelectAll, this.btnUnelectAll, this.btnInvertSelection]
+				items: [btnSelectAll, btnUnselectAll, btnInvertSelection]
 			})),
 			new OO.ui.FieldLayout(this.btnExecute)
 		]);
@@ -316,6 +341,207 @@ class RevdelForm {
 
 }
 
+class Revision {
+
+	/**
+	 * @param {HTMLLIElement} li
+	 */
+	constructor(li) {
+
+		const $li = $(li);
+
+		/**
+		 * The ID number of this revision.
+		 * @type {string}
+		 */
+		this.revid = $li.data('mw-revid');
+
+		const $pageLink = $li.find(isDeletedContribs ? '.mw-changeslist-title' : '.mw-contributions-title');
+		/**
+		 * The prefixed page name associated with this revision.
+		 * @type {string}
+		 */
+		this.pagename = (() => {
+			const href = $pageLink.attr('href');
+			let m;
+			if (href && (m = Revision.regex.article.exec(href) || Revision.regex.script.exec(href))) {
+				return decodeURIComponent(m[1]).replace(/_/g, ' ');
+			} else {
+				console.log($pageLink[0]);
+				throw new Error('The page link does not have a well-formed href.');
+			}
+		})();
+
+		/**
+		 * A span tag used to show the progress of revdel execution. This tag will contain an image or an error code.
+		 * @type {JQuery<HTMLInputElement>}
+		 */
+		this.$progress = $('<span>');
+
+		/**
+		 * @type {JQuery<HTMLInputElement>}
+		 */
+		this.$checkbox = $('<input>');
+
+		$li.prepend(
+			this.$progress
+				.addClass('mrd-progress'),
+			this.$checkbox
+				.prop('type', 'checkbox')
+				.addClass('mrd-checkbox')
+		);
+
+		/**
+		 * The date link. See {@link toggleContentVisibility} for all its HTML structures.
+		 * @type {JQuery<HTMLElement>} Usually an `<a>` tag, or a `<span>` tag when revdel-ed, or an empty jQuery object
+		 * when suppressed and the user doesn't have the suppressor rights.
+		 */
+		this.$date = (() => {
+			const $a = $li.find('a.mw-changeslist-date');
+			const $pr = $a.parent('span.' + Revision.class.deleted);
+			if ($pr.length) {
+				return $pr;
+			} else {
+				return $a;
+			}
+		})();
+
+		/**
+		 * Then `<span>` tag for summary. See {@link toggleCommentVisibility} for all its HTML structures.
+		 *
+		 * The tag is entirely missing on [[Special:DeletedContributions]] if the revision has no edit summary.
+		 * In this case, we create one here.
+		 *
+		 * @type {JQuery<HTMLSpanElement>}
+		 */
+		this.$comment = $li.children('.comment');
+		if (!this.$comment.length) {
+			this.$comment = $('<span>');
+			const clss = ['comment', 'comment--without-parentheses'];
+			if (isDeletedContribs) {
+				clss.pop();
+			}
+			$pageLink.after(
+				this.$comment.addClass(clss.join(' '))
+			);
+		}
+
+	}
+
+	/**
+	 * Set an icon and a text on its right in the progress tag.
+	 * @param {IconType?} icon The symbolic name of the icon to set, or `null` for no icon.
+	 * @param {string} [message] An optional message to display to the right of the icon.
+	 * @returns {Revision}
+	 */
+	setProgress(icon, message) {
+
+		this.$progress.empty();
+
+		let cls = '';
+		switch (icon) {
+			case 'doing':
+				this.$progress.append(getIcon(icon));
+				break;
+			case 'done':
+				cls = 'mrd-green';
+				this.$progress.append(getIcon(icon));
+				break;
+			case 'failed':
+				cls = 'mrd-red';
+				this.$progress.append(getIcon(icon));
+				break;
+			default:
+		}
+
+		if (typeof message === 'string') {
+			this.$progress.append(
+				$('<span>')
+					.text(message)
+					.addClass(cls)
+					.css('margin-left', '0.5em')
+			);
+		}
+
+		return this;
+
+	}
+
+	/**
+	 * Toggle the checked state of the checkbox.
+	 * @param {boolean} check
+	 * @returns {Revision}
+	 */
+	toggleSelection(check) {
+		this.$checkbox.prop('checked', check);
+		return this;
+	}
+
+	/**
+	 * Check if the checkbox is selected.
+	 * @returns {boolean}
+	 */
+	isSelected() {
+		return this.$checkbox.prop('checked');
+	}
+
+	/**
+	 * Toggle the revdel status of a date link.
+	 * ```
+	 *  // Normal date link
+	 *  <a class="mw-changeslist-date">2023-01-01T00:00:00</a>
+	 *  // Deleted date link
+	 *  <span class="history-deleted">
+	 *      <a class="mw-changeslist-date">2023-01-01T00:00:00</a>
+	 *  </span>
+	 *  // Suppressed date link (non-suppressor view; this function should never face this pattern)
+	 *  <span class="history-deleted mw-history-suppressed mw-changeslist-date"></span>
+	 * ```
+	 * Concerning the third pattern, a revision with any field suppressed can only be revision-deleted by suppressors.
+	 * This means that undeletable revisions should never be forwarded to the API in the first place.
+	 */
+	toggleContentVisibility() {
+
+	}
+
+	/**
+	 * Toggle the revdel status of a comment (edit summary).
+	 * ```
+	 *  // On [[Special:Contributions]]
+	 *  // Normal comment
+	 *  <span class="comment comment--without-parentheses">Some comment</span>
+	 *  // Deleted comment
+	 *  <span class="history-deleted comment">
+	 *      <span class="comment">(edit summary removed)</span>
+	 *  </span>
+	 *  // On [[Special:DeletedContributions]]
+	 *  // Normal comment (same as the topmost one)
+	 *  <span class="comment comment--without-parentheses">Some comment</span>
+	 *  // Deleted comment
+	 *  <span class="history-deleted comment">
+	 *      <span class="comment comment--without-parentheses">Some comment</span>
+	 *  </span>
+	 * ```
+	 * Note that when the comment is an empty string, there IS a span tag for comment with the 'mw-comment-none' class added
+	 * to pattern #1 on [[Special:Contributions]], but the tag is entirely missing on [[Special:DeletedContributions]]. This
+	 * script internally creates a comment element in createForm() if missing, so there's no problem.
+	 */
+	toggleCommentVisibility() {
+
+	}
+
+}
+
+Revision.regex = {
+	article: new RegExp(mw.config.get('wgArticlePath').replace('$1', '([^#?]+)')),
+	script: new RegExp(mw.config.get('wgScript') + '\\?title=([^#&]+)')
+};
+
+Revision.class = {
+	deleted: 'history-deleted',
+	suppressed: 'mw-history-suppressed'
+};
+
 /**
  * Get interface messages.
  * @param {string[]} namesArr
@@ -328,6 +554,30 @@ function getMessages(namesArr) {
 			console.error(err);
 			return {};
 		});
+}
+
+/**
+ * @typedef {'doing'|'done'|'failed'} IconType
+ */
+/**
+ * Get a loading/check/cross image tag.
+ * @param {IconType} iconType
+ * @returns {HTMLImageElement}
+ */
+function getIcon(iconType) {
+	const img = document.createElement('img');
+	switch (iconType) {
+		case 'doing':
+			img.src = '//upload.wikimedia.org/wikipedia/commons/4/42/Loading.gif';
+			break;
+		case 'done':
+			img.src = '//upload.wikimedia.org/wikipedia/commons/f/fb/Yes_check.svg';
+			break;
+		case 'failed':
+			img.src = '//upload.wikimedia.org/wikipedia/commons/a/a2/X_mark.svg';
+	}
+	img.style.cssText = 'vertical-align: middle; height: 1em; border: 0;';
+	return img;
 }
 
 //*********************************************************************************************
