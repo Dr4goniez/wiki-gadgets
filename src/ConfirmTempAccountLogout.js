0f0f9d4f6a61b6beca9f9b3be5168d975b610ebf
Dr4goniez <dragoniez.jawp@gmail.com>
2025-08-11 18:51:10 +0900

    CTAL v1.1.1: Add closure

--- src/ConfirmTempAccountLogout.js
+++ src/ConfirmTempAccountLogout.js
@@ -7,61 +7,65 @@
  *
  * Shows a confirmation (warning) popup when a temporary user attempts to log out.
  *
- * @version 1.1.0
+ * @version 1.1.1
  * @author [[User:Dragoniez]]
  */
-const moduleName = 'ext.gadget.selectorLogoutLink';
-$.when(
-	mw.loader.using([moduleName, 'mediawiki.util', 'oojs-ui']),
-	$.ready
-).then((req) => {
+(() => {
 
-	/**
-	 * @type {string}
-	 */
-	const selectorLogoutLink = req(moduleName);
-	/**
-	 * @type {JQuery<HTMLAnchorElement>}
-	 */
-	const $logout = $(selectorLogoutLink);
-	if (!$logout.length) {
-		return;
-	}
+	const moduleName = 'ext.gadget.selectorLogoutLink';
+	$.when(
+		mw.loader.using([moduleName, 'mediawiki.util', 'oojs-ui']),
+		$.ready
+	).then((req) => {
 
-	// Remove MediaWiki's default click handler that fires the logout hook
-	$logout.off('click');
+		/**
+		 * @type {string}
+		 */
+		const selectorLogoutLink = req(moduleName);
+		/**
+		 * @type {JQuery<HTMLAnchorElement>}
+		 */
+		const $logout = $(selectorLogoutLink);
+		if (!$logout.length) {
+			return;
+		}
 
-	// Warning message shown in a confirmation dialog
-	const rawLinks = {
-		scrutiny: `<a href="${mw.util.getUrl('WP:SCRUTINY')}" target="_blank">投稿記録の分断</a>`,
-		sock: `<a href="${mw.util.getUrl('WP:SOCK')}" target="_blank">多重アカウントの不正使用</a>`,
-		block: `<a href="${mw.util.getUrl('WP:BLOCK')}" target="_blank">投稿ブロック</a>`
-	};
-	const $warning = $('<div>').html(
-		'<h3 style="text-align:center;">警告</h3>' +
-		'<div style="text-align:justify;">' +
-			'<p>' +
-				'ご利用中の端末を今後も継続して使用する場合、通常は仮アカウントからログアウトする必要はありません。' +
-				'一度ログアウトすると、同じ仮アカウントに再度ログインすることはできなくなります。今後も編集を行う場合は、' +
-				'ログイン状態を維持してください。' +
-			'</p>' +
-			'<p>' +
-				`ログアウトする場合は、${rawLinks.scrutiny}を招かないよう十分に注意してください。${rawLinks.sock}と` +
-				`みなされた場合、${rawLinks.block}の対象となることがあります。` +
-			'</p>' +
-			'<p>本当にログアウトしますか？</p>' +
-		'</div>'
-	);
+		// Remove MediaWiki's default click handler that fires the logout hook
+		$logout.off('click');
 
-	$logout.on('click', function(event) {
-		event.preventDefault(); // Cancel default anchor navigation
+		// Warning message shown in a confirmation dialog
+		const rawLinks = {
+			scrutiny: `<a href="${mw.util.getUrl('WP:SCRUTINY')}" target="_blank">投稿記録の分断</a>`,
+			sock: `<a href="${mw.util.getUrl('WP:SOCK')}" target="_blank">多重アカウントの不正使用</a>`,
+			block: `<a href="${mw.util.getUrl('WP:BLOCK')}" target="_blank">投稿ブロック</a>`
+		};
+		const $warning = $('<div>').html(
+			'<h3 style="text-align:center;">警告</h3>' +
+			'<div style="text-align:justify;">' +
+				'<p>' +
+					'ご利用中の端末を今後も継続して使用する場合、通常は仮アカウントからログアウトする必要はありません。' +
+					'一度ログアウトすると、同じ仮アカウントに再度ログインすることはできなくなります。今後も編集を行う場合は、' +
+					'ログイン状態を維持してください。' +
+				'</p>' +
+				'<p>' +
+					`ログアウトする場合は、${rawLinks.scrutiny}を招かないよう十分に注意してください。${rawLinks.sock}と` +
+					`みなされた場合、${rawLinks.block}の対象となることがあります。` +
+				'</p>' +
+				'<p>本当にログアウトしますか？</p>' +
+			'</div>'
+		);
 
-		OO.ui.confirm($warning, { size: 'larger' }).then((confirmed) => {
-			if (confirmed) {
-				// Re-fire the logout hook with the href manually
-				mw.hook('skin.logout').fire(this.href);
-			}
+		$logout.on('click', function(event) {
+			event.preventDefault(); // Cancel default anchor navigation
+
+			OO.ui.confirm($warning, { size: 'larger' }).then((confirmed) => {
+				if (confirmed) {
+					// Re-fire the logout hook with the href manually
+					mw.hook('skin.logout').fire(this.href);
+				}
+			});
 		});
+
 	});
 
-});
+})();
